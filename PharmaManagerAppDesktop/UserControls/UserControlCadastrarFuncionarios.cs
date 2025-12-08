55958b714d00c14498fefbc9880260f59fde731a
Domingos Nkula Pedro <2001.dnp@gmail.com>
2025-12-08 16:10:55 +0100

    Feito

---
+++ PharmaManagerAppDesktop/UserControls/UserControlCadastrarFuncionarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmaManagerAppDesktop.UserControls
+{
+    public partial class UserControlCadastrarFuncionarios : UserControl
+    {
+        public UserControlCadastrarFuncionarios()
+        {
+            InitializeComponent();
+        }
+
+        private void checkUsuario_CheckedChanged(object sender, EventArgs e)
+        {
+            // Checar se o funcionário possuirá uma conta de Login
+            if (checkUsuario.Checked == true)
+            {
+                panelDadosDeAcesso.Visible = true;
+            }
+            else
+            {
+                panelDadosDeAcesso.Visible = false;
+                tbSenha.Clear();
+                tbConfirmarSenha.Clear();
+            }
+        }
+    }
+}
