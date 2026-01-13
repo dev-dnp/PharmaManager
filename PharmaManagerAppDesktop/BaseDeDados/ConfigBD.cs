c715602d08eb1ba865f5f16d216ddacb3b83f031
Domingos Nkula Pedro <2001.dnp@gmail.com>
2026-01-13 06:00:36 +0100

    Alteração da string de conexão

--- PharmaManagerAppDesktop/BaseDeDados/ConfigBD.cs
+++ PharmaManagerAppDesktop/BaseDeDados/ConfigBD.cs
@@ -20,17 +20,15 @@ namespace PharmaManagerAppDesktop.BaseDeDados
         /* ----------------------------------------------------------------------
             ATRIBUTO ESTÁTICO QUE SERÁ PARTILHANDO IGUALMENTE POR TODO SISTEMA
            ---------------------------------------------------------------------- */
-        public static string StringConexao { get; set; } = @$"
-                                                            Data Source=localhost,1500;
-                                                            Initial Catalog=PHARMA_MANAGER;
-                                                            User ID=sa;
-                                                            Password=MinhaSenha!123;
-                                                            Persist Security Info=False;
-                                                            Pooling=False;
-                                                            MultipleActiveResultSets=False;
-                                                            Encrypt=True;
-                                                            TrustServerCertificate=True;
-                                                            Application Name=SQL Server Management Studio";
+        public static string StringConexao { get; set; } = @"   Data Source=localhost;
+                                                                Integrated Security=True;
+                                                                Persist Security Info=False;
+                                                                Pooling=False;
+                                                                Database = PHARMA_MANAGER;
+                                                                MultipleActiveResultSets=False;
+                                                                Encrypt=True;
+                                                                TrustServerCertificate=True;
+                                                                Application Name=""SQL Server Management Studio"";";
 
     }
 }
